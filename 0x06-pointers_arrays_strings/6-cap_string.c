9cc6f28d5a820144cf2c76339f7f347c6767a5ed
Essam-Kamal <essam.k.mu@gmail.com>
2023-09-21 16:50:12 -0700

    Capitalizing all letters of a string

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -50,7 +50,7 @@ char *cap_string(char *s)
 	{
 		if (isDelimiter(*s))
 			foundDelimit = 1;
-		else if (islower(*s) && foundDelimit)
+		else if (isLower(*s) && foundDelimit)
 		{
 			*s -= 32;
 			foundDelimit = 0;
