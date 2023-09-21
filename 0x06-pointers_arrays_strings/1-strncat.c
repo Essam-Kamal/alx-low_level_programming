66e09f98b486a9188795871b335b1c650c5123dd
Essam-Kamal <essam.k.mu@gmail.com>
2023-09-21 15:02:11 -0700

    Function that concatenates two strings

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -5,11 +5,12 @@
  *
  * @dest: pointer to destination point
  * @src: pointer to source input
+ * @n: most number of bytes from @src
  *
  * Return: pointer to resulting string @dest
 */
 
-char *_strcat(char *dest, char *src)
+char *_strncat(char *dest, char *src, int n);
 {
 	int c, i;
 
@@ -19,7 +20,7 @@ char *_strcat(char *dest, char *src)
 		c++;
 
 	for (i = 0; i < n && src[i] != '\0'; i++)
-		dest[c + i] = src [i];
+		dest[c + i] = src[i];
 
 	dest[c + i] = '\0';
 
