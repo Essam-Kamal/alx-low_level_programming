f5248afbd1d2b2359f1da1a0f6f1f034a3792127
Essam-Kamal <essam.k.mu@gmail.com>
2023-10-06 14:23:59 -0700

    Function that copies memory area.

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -5,16 +5,16 @@
  * @s: pointer to put the constant
  * @b: constant
  * @n: max bytes to use
- * Return: pointer s
+ * Return: s
 */
 
 char *_memset(char *s, char b, unsigned int n)
 {
-	unsigned int itr;
+	unsigned int i;
 
-	for (itr = 0; n > 0; itr++, n--)
+	for (i = 0; n > 0; i++, n--)
 	{
-		s[itr] = b;
+		s[i] = b;
 	}
 
 	return (s);
