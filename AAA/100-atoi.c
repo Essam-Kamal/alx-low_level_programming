96cec4deafda572cc656560c48c561da6459e225
Essam-Kamal <essam.k.mu@gmail.com>
2023-10-06 18:12:59 -0700

    Create the static library libmy.a containing all the functions

--- AAA/100-atoi.c
+++ AAA/100-atoi.c
@@ -11,40 +11,17 @@
 
 int _atoi(char *s)
 {
-	int result = 0;
+	unsigned int num = 0;
 	int sign = 1;
-	int i = 0;
 
-	while (s[i] == ' ' || s[i] == '\t' || s[i] == '\n')
-		i++;
+	do {
+		if (*s == '-')
+			sign *= -1;
+		else if (*s >= '0' && *s <= '9')
+			num = (num * 10) + (*s - '0');
+		else if (num > 0)
+			break;
+	} while (*s++);
 
-	if (s[i] == '-')
-	{
-		sign = -1;
-		i++;
-	}
-
-	else if (s[i] == '+')
-	{
-		i++;
-	}
-
-	while (s[i] >= '0' && s[i] <= '9')
-	{
-		if (result > INT_MAX / 10 ||
-			(result == INT_MAX / 10 && (s[i] - '0') > (INT_MAX % 10)))
-		{
-			if (sign == 1)
-				return (INT_MAX);
-
-			else
-
-				return (INT_MIN);
-		}
-
-		result = result * 10 + (s[i] - '0');
-		i++;
-	}
-
-	return (result * sign);
+	return (num * sign);
 }
